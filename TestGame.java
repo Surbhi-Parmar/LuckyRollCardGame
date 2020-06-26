861ce8d01d52ed1ad5a07bfc49c171043c6acabd
Surbhi-Parmar <65972144+Surbhi-Parmar@users.noreply.github.com>
2020-06-25 20:20:02 -0400

    Update TestGame.java

--- TestGame.java
+++ TestGame.java
@@ -1,62 +1,107 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
-
-/**
- * A concrete class that represents any grouping of cards for a Game.
- * HINT, you might want to subclass this more than once.
- * The group of cards has a maximum size attribute which is flexible for reuse.
- * @author surbhi parmar, 2020
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package ca.sheridancollege.project;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
-public class TestGame 
+
+/**
+ *
+ * @author surbhi parmar,2020
+ */
+    
+public class Card implements Comparable<Card>
 {
-   
-  public TestGame()
+     private Card()
         {
-
         }
 
-        public static void main(String[] args)
+        public enum CARDNUMBER
         {
-                CardGame sl = new CardGame();
+                TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(
+                                14);
 
-                System.out.println("Card Game \n Player Options");
-                System.out.println("1. Start Game \n  \n2. Exit Game");
-                System.out.print("Please provide your option : ");
+                private int     ord;
 
-                int i = 1;
-
-                while (i != 0)
+                private CARDNUMBER(int i)
                 {
-                        Scanner in = new Scanner(System.in);
-                        i = in.nextInt();
+                        this.ord = i;
+                }
 
-                        switch (i)
-                        {
-                                case 1:
-                                        System.out.println("Provide the Number of Players( should be greater than 1 and less than 4) : ");
-                                        in = new Scanner(System.in);
-                                        i = in.nextInt();
-                                        sl.playGame(i);
-
-                                        sl.displayWinners();
-                                        break;
-
-                                case 2:
-                                        System.exit(0);
-                        }
-
-                        System.out.println();
-                        System.out.println("Card Game \n Select User Options");
-                        System.out.println("1. Start Game \n2. Exit Game");
-                        System.out.print("Please provide your option : ");
+            
+                public int getOrd()
+                {
+                        return ord;
                 }
         }
+
+        public enum CARDTYPE
+        {
+                CLUB, DIAMOND, HEARTS, SPADE;
+        }
+
+        private CARDNUMBER      cdNumber;
+        private CARDTYPE        cdType;
+
+        public CARDNUMBER getCdNumber()
+        {
+                return cdNumber;
+        }
+
+        public CARDTYPE getCdType()
+        {
+                return cdType;
+        }
+
+        public static List<Card> getPackOfCards()
+        {
+                List<Card> crdLst = new ArrayList<Card>();
+
+                for (CARDTYPE types : CARDTYPE.values())
+                {
+                        for (CARDNUMBER cNums : CARDNUMBER.values())
+                        {
+                                Card cd = new Card();
+                                cd.cdNumber = cNums;
+                                cd.cdType = types;
+                                crdLst.add(cd);
+                        }
+                }
+                return crdLst;
+        }
+
+        public static void shuffleCards(List<Card> cards)
+        {
+                Collections.shuffle(cards);
+        }
+
+        @Override
+        public int compareTo(Card o)
+        {
+                if (this.getCdNumber() == o.getCdNumber())
+                {
+                        return 0;
+                }
+                else if (this.getCdNumber().getOrd() > o.getCdNumber().getOrd())
+                {
+                        return 1;
+                }
+                else
+                        return -1;
+        }
+
+        @Override
+        public String toString()
+        {
+                return "CARD [cdNumber=" + cdNumber + ", cdType=" + cdType + "]";
+        }
+}
     
-}//end class
+
+
+    
+
