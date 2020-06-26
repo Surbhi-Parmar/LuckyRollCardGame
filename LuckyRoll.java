dd4f8c1e5de5a733b1d2cd45a16dab5ed6b4243b
Surbhi-Parmar <65972144+Surbhi-Parmar@users.noreply.github.com>
2020-06-25 20:18:31 -0400

    Update LuckyRoll.java

--- LuckyRoll.java
+++ LuckyRoll.java
@@ -1,7 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ca.sheridancollege.project;
-public interface Game
+
+/**
+ *
+ * @author surbhi parmar
+ */
+public class LuckyRoll{
+    public interface Game
 {
         void playGame(int numberOfPlayers);
 
         void displayWinners();
 }
+}
+
